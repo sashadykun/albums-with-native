db1233bc6b24935afc799ae00a45974f3ef75ee9
SD <sashaluckyman@gmail.com>
2019-04-08 15:02:13 -0700

    add style to card component and referance to image

--- src/components/album_detail.js
+++ src/components/album_detail.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './card';
 import CardSection from './card_section';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+    const { title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headerContentStyle } = styles;
+
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image style={thumbnailStyle} source={{uri: thumbnail_image}} />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+                
             </CardSection>
         </Card>
     );
 };
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
+}
 
 
 export default AlbumDetail;
